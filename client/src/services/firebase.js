9afd9eab23d844621c4f8c5d662af57a9374bc60
adilelmoba <adilelmoba20@gmail.com>
2023-03-08 20:48:44 +0100

    auth

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export const database = getFirestore(app);
+export const auth = getAuth(app);
